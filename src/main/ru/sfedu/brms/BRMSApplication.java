7581318b9ad724a8f5406ffaedee955b5a401c07
Sergey Petryaev <spetryaev@bostonsd.ru>
2019-11-16 12:48:21 +0300

    fix

--- src/main/ru/sfedu/brms/BRMSApplication.java
+++ src/main/ru/sfedu/brms/BRMSApplication.java
@@ -6,25 +6,6 @@ import ru.sfedu.brms.service.BankDepositService;
 public class BRMSApplication {
 
     public static void main(String[] args) {
-        BankDepositService depositService = new BankDepositService();
 
-        try {
-            BankDeposit deposit = new BankDeposit();
-            deposit.setAmount(1_000_000);
-            deposit.setTerm(24);
-            depositService.applyInterestToDeposit(deposit);
-
-            deposit.setTerm(12);
-            depositService.applyInterestToDeposit(deposit);
-
-            deposit.setAmount(500_000);
-            depositService.applyInterestToDeposit(deposit);
-
-            deposit.setAmount(500_000);
-            deposit.setTerm(24);
-            depositService.applyInterestToDeposit(deposit);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
